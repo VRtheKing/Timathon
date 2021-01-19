45c99f67e57dd4ce79e4bce83ec3fd2a856088c6
Tuladhar <tuladharreddy345@gmail.com>
2021-01-19 20:01:07 +0530

    added yt and google api

--- commands.py
+++ commands.py
@@ -1,7 +1,7 @@
 import speech_recognition as sr
 import speak
-from youtube3 import YoutubeClient 
-
+from youtube3 import YoutubeClient
+import wikipedia
 from datetime import datetime
 import pyautogui
 
@@ -27,20 +27,20 @@ def take_commands():
         except Exception as e:
             print(e)
             speak.speak('retry')
-       
+
 
 #speak_commands
 class speak_commands():
     def __init__(self,command):
         self.command = command
-    
+
         if 'new' in self.command:
             x = self.command.replace('new','')
             print(x)
 
             if 'hello' in x:
                 speak.speak('hello, What can i do for you')
-            
+
             elif 'youtube' in x:
                 x = self.command.replace('new youtube','')
                 play = 'playing'+ x
@@ -50,7 +50,7 @@ class speak_commands():
             elif 'time' in x:
                 current_time = now.strftime("%H:%M")
                 speak.speak("Current Time is" + current_time)
-            
+
             elif 'search' in x:
                 x = self.command.replace('new search','')
                 searching = 'searching' + x
@@ -58,10 +58,15 @@ class speak_commands():
                 speak.speak(searching)
                 search(x)
 
-        
+            elif 'wiki' in x:
+                x = self.commands.replace('new wiki', '')
+                print(wikipedia.summary(x, sentences = 3))
+                speak.speak(wikipedia.summary(x, sentences = 3))
+
+
     def playonyt(self,vid):
         playonyt(vid)
 
 
-command = take_commands()     
+command = take_commands()
 speak_command = speak_commands(command)
