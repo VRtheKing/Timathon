6e3082fc7871a87de330f2632bd82b535670dc55
Tuladhar <tuladharreddy345@gmail.com>
2021-01-25 20:56:26 +0530

    error fixed

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
+import backend_api
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['POST','GET'])
 def home():
+	if request.method == 'POST':
+		x = request.form['text']
+		backend_api.start(x)
 	return render_template("index.html")
 
 
